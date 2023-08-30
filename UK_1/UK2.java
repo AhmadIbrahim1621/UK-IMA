03e0c3c92d9327ab95e902b050279ed0fe1040e8
AhmadIbrahim1621 <ahmadmubaroq1621@gmail.com>
2023-08-31 04:24:42 +0700

    Commit

--- UK_1/UK2.java
+++ UK_1/UK2.java
@@ -14,19 +14,16 @@ public class UK2 {
     }
     public static String tafsirPesan(String pesanEnkripsi) {
         String[] kataKode = pesanEnkripsi.split(" ");
-        String hurufVokal = "AIUEO";
         StringBuilder pesanTafsir = new StringBuilder();
-        String hurufKonsonan = "BCDFGHJKLMNPQRSTVWXYZ";
 
         for (String kata : kataKode) {
-            if (kata.length() < 2) {
+            if (kata.length() >= 4) {
                 pesanTafsir.append(kata).append(" ");
                 continue;
             }
-            char hurufVokalPertama = kata.charAt(kata.length() - 2);
-            char hurufVokalKedua = kata.charAt(kata.length() - 1);
+            char hurufAr = kata.charAt(kata.length() - 3);
 
-            if (hurufVokal.indexOf(hurufVokalPertama, hurufVokalKedua) != -1 && hurufKonsonan.indexOf(hurufVokalKedua) != -1) {
+            if ("AEIOUaeiou".indexOf(hurufAr) != -1) {
                 pesanTafsir.append(kata.substring(0, kata.length() -2)).append(" ");
             } else {
                 pesanTafsir.append(kata).append(" ");
